cca970af782dccdbf91540935566a738e78a598a
devsujalpatel <sujal7455@gmail.com>
2025-09-07 19:08:27 +0530

    few Changes

--- src/components/sign-up-form.tsx
+++ src/components/sign-up-form.tsx
@@ -4,18 +4,18 @@ import axios from "axios";
 export const SignUpForm = () => {
   const submit = async () => {
     try {
-      const response = await fetch("http://localhost:8000/api/users/register", {
+      const response = await fetch("https://bauna.me/api/users/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          firstname: "John", // ✅ must match backend schema
+          firstname: "John", 
           lastname: "Doe",
           email: "gB0wW@example.com",
           password: "password123",
         }),
-        credentials: "include", // if you need cookies
+        credentials: "include",
       });
 
       const data = await response.json();
